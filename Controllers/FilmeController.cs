3cd51d910e0e8c54f62766a257ead29df038681c
Willian da Silva Almeida <williandasilvaalmeida3@gmail.com>
2022-02-18 09:48:19 -0300

    Adicionando os metodos get, e adicionando pacotes nuget para persistencia com o banco.

--- Controllers/FilmeController.cs
+++ Controllers/FilmeController.cs
@@ -2,22 +2,42 @@
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace FilmesAPI.Controllers
 {
 
     [ApiController]
     [Route("[controller]")]
-    public class FilmeController: ControllerBase
+    public class FilmeController : ControllerBase
     {
         private static List<Filme> filmes = new List<Filme>();
+        private static int Id = 1;
 
         [HttpPost]
-        public void AdicionaFilme([FromBody]Filme filme)
+        public IActionResult AdicionaFilme([FromBody] Filme filme)
         {
-
+            filme.Id = Id++;
             filmes.Add(filme);
-            
+            return CreatedAtAction(nameof(RecuperaFilmePorId), new {Id = filme.Id}, filme);
+
+        }
+
+        [HttpGet]
+        public IActionResult RecuperarFilmes()
+        {
+            return Ok(filmes);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult RecuperaFilmePorId(int id)
+        {
+             Filme filme = filmes.FirstOrDefault(filme => filme.Id == id);
+            if (filme != null)
+            {
+                Ok(filme);  // 200            
+            }
+            return NotFound(); //404
         }
 
     }
